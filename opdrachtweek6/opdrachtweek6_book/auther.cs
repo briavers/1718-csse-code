786fc32ff54ade45688a5fab50b6deec1f5a6819
briavers <briavers@student.arteveldehs.be>
2017-11-16 12:59:34 +0100

    opdracht week 6

---
+++ opdrachtweek6/opdrachtweek6_book/auther.cs
@@ -0,0 +1,13 @@
+public class Auther : Book
+{
+  string name;
+  string email;
+  char gender;
+
+  public Auther(string name, string email, char gender)
+  {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
+}
---
+++ opdrachtweek6/opdrachtweek6_book/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace weeek06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book voorbeeld = new Book('theNames', 8, 23);
+
+        }  
+    }
+}
